de82575551b3921409dfde0e8b69be4c5029a3d8
Fer <pxocktopus@gmail.com>
2026-02-04 21:44:06 +0100

    setting up templates

---
+++ app_ingresos_gastos/routes.py
@@ -0,0 +1,5 @@
+from app_ingresos_gastos import app
+from flask import render_template
+@app.route("/")
+def index():
+    return render_template("index.html")
---
+++ main.py
@@ -0,0 +1,7 @@
+from app_ingresos_gastos import app
+
+#Inicia server con:
+#set FLASK_APP=main.py
+
+#correr en modo debug el server con:
+#flask --app main --debug run
