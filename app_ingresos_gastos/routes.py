b4fd3d7a37f05b47019b6c45ece668beb640e56e
Fer <pxocktopus@gmail.com>
2026-02-05 23:01:28 +0100

    updating list using jinja instead of using plane html

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -3,8 +3,13 @@ from flask import render_template
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    datos = [
+        {"fecha":"01/09/2025","concepto":"Salario","monto":1800},
+        {"fecha":"15/09/2025","concepto":"Compras de Alimentos","monto":-250},
+        {"fecha":"30/09/2025","concepto":"Compra de Ropa","monto":-150}
+    ]
+    return render_template("index.html", title=" Lista",lista = datos)
 
 @app.route("/new")
 def new():
-    return render_template("new.html")
+    return render_template("new.html",title=" Registro")
