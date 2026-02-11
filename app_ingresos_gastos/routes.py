1914d8a12ecad377fcfe3edbe97e93452aeafa47
Fer <pxocktopus@gmail.com>
2026-02-11 21:01:18 +0100

    updated version adding the routes to edit and delete, and also added an ID for our .csv lines

--- app_ingresos_gastos/routes.py
+++ app_ingresos_gastos/routes.py
@@ -23,10 +23,11 @@ def index():
 @app.route("/new",methods=["GET","POST"])
 def new():
     if request.method == "POST":
-        fecha_actual = str(date.today())
         
-        if request.form["fecha"] > fecha_actual:
-            return render_template("new.html",title=" Registro",titulo="Registro",boton="Guardar")
+        comprobar_errores = validar_formulario(request.form)
+        if comprobar_errores:
+            return render_template("new.html", title="Registro", titulo="Registro",boton="Guardar",error=comprobar_errores,dataform = request.form)
+
         else:
             fichero = open("app_ingresos_gastos/data/movimientos.csv","a",newline="")
             lectura = csv.writer(fichero, delimiter=",", quotechar='"')
@@ -36,12 +37,25 @@ def new():
         return redirect("/")
     
     else:
-        return render_template("new.html",title=" Registro",titulo="Registro",boton="Guardar")
+        return render_template("new.html",title=" Registro",titulo="Registro",boton="Guardar",dataform = {})
 
-@app.route("/delete")
-def delete():
+@app.route("/delete/<int:id>")
+def delete(id):
     return render_template("delete.html",title="Borrar")
 
-@app.route("/update")
-def uptade():
+@app.route("/update/<int:id>")
+def update(id):
     return render_template("update.html",title="Actualizar",titulo="Actualización",boton="Actualizar")
+
+
+def validar_formulario(datos_formulario):
+    hoy = str(date.today())
+    errores = []
+    if datos_formulario["fecha"] > hoy:
+        errores.append("La fecha no puede ser mayor que la actual")
+    if datos_formulario["concepto"] == "":
+        errores.append("El concepto no puede ir vacío")
+    if datos_formulario["monto"] == "" or float(datos_formulario["monto"]) == 0:
+        errores.append("El monto debe ser diferente de 0 o no estar vacío")
+            
+    return errores
